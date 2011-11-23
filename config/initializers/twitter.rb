05944b7c0bdea364df9af687c956621c35273b4f
Brian Ryckbost <bryckbost@gmail.com>
2011-11-23 14:18:36 -0500

    Add pre initializer to load configuration from application.yml

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,4 +1,4 @@
 Twitter.configure do |config|
-  config.consumer_key = ENV["CONSUMER_KEY"]
-  config.consumer_secret = ENV["CONSUMER_SECRET"]
+  config.consumer_key = ENV["TWITTER_CONSUMER_KEY"]
+  config.consumer_secret = ENV["TWITTER_CONSUMER_SECRET"]
 end

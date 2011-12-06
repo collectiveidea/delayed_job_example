3063d10799a5b20512b0bb85d7c1488e8259c06f
Brian Ryckbost <bryckbost@gmail.com>
2011-12-06 09:52:39 -0500

    After a message is saved, schedule a job to run in the future at the specified publish at time

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,19 @@ class Message < ActiveRecord::Base
   
   validates :body, :presence => true
   
+  after_save :schedule_tweet
+  
+  def publish_at=(datetime)
+    self[:publish_at] = ChronicDuration.parse(datetime)
+  end
+  
+  private
+  def schedule_tweet
+    if Time.zone.now >= publish_at
+      client = Twitter::Client.new(:oauth_token => user.twitter_access_token, :oauth_token_secret => user.twitter_access_secret)
+      client.update(body)
+    end
+  end
+  handle_asynchronously :schedule_tweet, :run_at => Proc.new {|p| p.publish_at }
+  
 end
